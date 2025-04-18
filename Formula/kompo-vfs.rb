fc7fb1fd7e6c7b162401caf721078bf134411e23
megane42 <h.kzm.gm@gmail.com>
2025-04-18 23:00:43 +0900

    try to configure formula

--- Formula/kompo-vfs.rb
+++ Formula/kompo-vfs.rb
@@ -1,8 +1,8 @@
 class KompoVfs < Formula
   desc "Virtual filesystem library for kompo gem"
-  homepage "https://github.com/ahogappa0613/kompo-vfs"
-  url "https://github.com/ahogappa0613/kompo-vfs.git", using: :git, branch: "main"
-  head "https://github.com/ahogappa0613/kompo-vfs.git", branch: "main"
+  homepage "https://github.com/megane42/kompo-vfs"
+  url "https://github.com/megane42/kompo-vfs.git", using: :git, branch: "feature/fix-compile-error-on-mac"
+  head "https://github.com/megane42/kompo-vfs.git", branch: "feature/fix-compile-error-on-mac"
   version "0.2.0"
 
   depends_on "rust" => :build
